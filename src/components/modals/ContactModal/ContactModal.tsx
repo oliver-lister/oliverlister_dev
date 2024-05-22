4c2e840869688fb728785052dfc41810a6396df8
oliver-lister <oliverlister98@gmail.com>
2024-05-22 20:23:31 +1000

    Fix modal styling - No more excessive padding on mobile screens

--- src/components/modals/ContactModal/ContactModal.tsx
+++ src/components/modals/ContactModal/ContactModal.tsx
@@ -3,7 +3,7 @@
 import ContactForm, {
   ContactFormData,
 } from "@/components/modals/ContactModal/ContactForm/ContactForm";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ModalExitButton from "../components/ModalExitButton";
 import { IconMail } from "@tabler/icons-react";
 import ContactFormSubmission from "./ContactForm/ContactFormSubmission";
@@ -29,7 +29,7 @@ const ContactModal = ({ closeModal }: { closeModal: () => void }) => {
 
   const dialogClass = `${
     isMounted ? "translate-y-0" : "translate-y-[-100vh]"
-  } transition-transform duration-300 ease-in-out rounded-xl max-w-screen-sm w-11/12 md:w-10/12 overflow-y-scroll h-[90%] sm:h-auto p-6 bg-secondary dark:bg-primary`;
+  } transition-transform duration-300 ease-in-out rounded-xl max-w-screen-sm w-11/12 md:w-10/12 max-h-screen overflow-y-scroll p-6 bg-secondary dark:bg-primary`;
 
   return (
     <div
