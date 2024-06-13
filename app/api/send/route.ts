82be80c867fdfb4b8ac060b15052944cf70136b3
oliver-lister <oliverlister98@gmail.com>
2024-06-13 16:28:52 +1000

    fixed resend type errors

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from "@/components/EmailTemplate";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
