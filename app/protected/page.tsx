ecbea0a47c3af5d3f83a59bd54b93164a875ea80
oliver-lister <oliverlister98@gmail.com>
2024-07-12 09:32:43 +1000

    removed links to non-existent modules

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,6 @@
 import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/libs/utils/supabase/server";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
@@ -31,7 +30,6 @@ export default async function ProtectedPage() {
       </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
         </main>
